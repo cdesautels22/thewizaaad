302f575eea7254bad92cb9165436c0a180b1104e
Cory Desautels <corydesautels@gmail.com>
2016-05-15 12:53:19 -0700

    send everything through babel

--- application/views/citadels.php
+++ application/views/citadels.php
@@ -11,14 +11,13 @@ $this->load->helper('url');
   <script src="<?php echo base_url();?>js/lib/react.js"></script>
   <script src="<?php echo base_url();?>js/lib/react-dom.js"></script>
   <script src="<?php echo base_url();?>js/lib/redux.js"></script>
-  <script src="<?php echo base_url();?>js/lib/redux-thunk.js"></script>
   <script src="<?php echo base_url();?>js/lib/react-redux.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
 </head>
 <body>
 
   <div id="root"></div>
-  <script src="<?php echo base_url();?>js/index.js"></script>
+  <script type="text/babel" src="<?php echo base_url();?>js/index.js"></script>
 
 </body>
 </html>
