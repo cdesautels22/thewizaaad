e0ba9a8e83a040106be9451327d03f4400f729b5
Cory Desautels <corydesautels@gmail.com>
2016-05-15 12:07:35 -0700

    include react

--- application/views/citadels.php
+++ application/views/citadels.php
@@ -1,11 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+$this->load->helper('url');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>Citdels</title>
+  <script src="<?php echo base_url();?>js/lib/react.js" ></script>
+  <script src="<?php echo base_url();?>js/lib/react-dom.js" ></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
 </head>
 <body>
 
