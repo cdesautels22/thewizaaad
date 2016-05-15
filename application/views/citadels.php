11f2fb4cebca585e56afc95f9fa4fa2056f8b669
Cory Desautels <corydesautels@gmail.com>
2016-05-15 12:51:56 -0700

    use index js

--- application/views/citadels.php
+++ application/views/citadels.php
@@ -8,21 +8,17 @@ $this->load->helper('url');
 <head>
 	<meta charset="utf-8">
 	<title>Citdels</title>
-  <script src="<?php echo base_url();?>js/lib/react.js" ></script>
-  <script src="<?php echo base_url();?>js/lib/react-dom.js" ></script>
-  <script src="<?php echo base_url();?>js/lib/redux.js" ></script>
-  <script src="<?php echo base_url();?>js/lib/react-redux.js" ></script>
+  <script src="<?php echo base_url();?>js/lib/react.js"></script>
+  <script src="<?php echo base_url();?>js/lib/react-dom.js"></script>
+  <script src="<?php echo base_url();?>js/lib/redux.js"></script>
+  <script src="<?php echo base_url();?>js/lib/redux-thunk.js"></script>
+  <script src="<?php echo base_url();?>js/lib/react-redux.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
 </head>
 <body>
 
   <div id="root"></div>
-  <script type="text/babel">
-    ReactDOM.render(
-      <h1>Hello, world!</h1>,
-      document.getElementById('root')
-    );
-  </script>
+  <script src="<?php echo base_url();?>js/index.js"></script>
 
 </body>
 </html>
